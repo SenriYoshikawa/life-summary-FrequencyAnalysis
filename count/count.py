b06d740ff61b19f3174b526008fa3d50def4b02b
SenriYoshikawa <tisato8899@gmail.com>
2017-10-13 00:14:58 +0900

    穴埋めの上限を12時間にした

--- count/count.py
+++ count/count.py
@@ -9,6 +9,7 @@ for i in range(1, len(sys.argv)):
     reader = csv.reader(infile)
 
     data_list = []
+    result = [[]]
     activeDays = [0 for i in range(32)]
     pre_date = next(reader)[0]
     infile.seek(0)
@@ -18,11 +19,11 @@ for i in range(1, len(sys.argv)):
 
         if pre_date[0:-3] != date[0:-3]:
 
-            # 一定以下の居間の0は1とみなす
+            # 12時間未満の居間1に含まれた居間玄関0は居間1とみなす
             for j in range(len(data_list) - 1):
-                if data_list[j] == 1 and data_list[j+1] == 0:
+                if data_list[j] == 1 and data_list[j + 1] == 0:
                     j += 1
-                    for k in range(min(60, len(data_list) - j)):
+                    for k in range(min(1440, len(data_list) - j)):
                         if data_list[j + k] == -1:
                             j += k
                             break
@@ -40,23 +41,23 @@ for i in range(1, len(sys.argv)):
 
             # 毎月1日の午前８時から午後８時をサンプルとして描画
             plt.subplot(221)
-            plt.xticks([0, 320, 640, 960],["8","12","16","20"])
-            plt.plot(data_list[960:1920], linewidth = 0.1)
+            plt.xticks([0, 320, 640, 960], ["8", "12", "16", "20"])
+            plt.plot(data_list[960:1920], linewidth=0.1)
             plt.title("day 1")
 
             # 一ヶ月
             plt.subplot(222)
-            plt.plot(data_list, linewidth = 0.1)
+            plt.plot(data_list, linewidth=0.1)
             plt.title("A month activity volume")
             plt.xticks([0, 20160, 40320, 60480, 80640], ["1", "7", "14", "21", "28"])
             plt.ylim(-2, 2)
-            plt.text(50000,1.5, str(activeDays.count(1)) + " active days", fontsize = 7)
+            plt.text(50000, 1.5, str(activeDays.count(1)) + " active days", fontsize=7)
 
             # データがある月はFFTを計算
             if activeDays.count(1) != 0:
                 F = np.fft.fft(data_list)
-                freqList = np.fft.fftfreq(len(F), d=1.0 / 2)[0:math.ceil(len(F)/2)]
-                FAbs = np.abs(F)[0:math.ceil(len(F)/2)] / activeDays.count(1)
+                freqList = np.fft.fftfreq(len(F), d=1.0 / 2)[0:math.ceil(len(F) / 2)]
+                FAbs = np.abs(F)[0:math.ceil(len(F) / 2)] / activeDays.count(1)
 
                 # FFT結果の絶対値
                 plt.subplot(223)
@@ -69,14 +70,13 @@ for i in range(1, len(sys.argv)):
                 FAbs[np.isnan(FAbs)] = 0
                 q = math.ceil(len(FAbs) / 4)
 
-                quarter = np.array([math.ceil(sum(FAbs[0:q])),
-                                   math.ceil(sum(FAbs[q:q*2])),
-                                   math.ceil(sum(FAbs[q*2:q*3])),
-                                   math.ceil(sum(FAbs[q*3:q*4]))])
+                each_sum = [math.ceil(sum(FAbs[0:q])), math.ceil(sum(FAbs[q:q * 2])), math.ceil(sum(FAbs[q * 2:q * 3])),
+                            math.ceil(sum(FAbs[q * 3:q * 4]))]
+                quarter = np.array(each_sum)
                 plt.pie(quarter,
-                        labels = ["q1", "q2", "q3", "q4"],
-                        counterclock = False,
-                        startangle = 90,
+                        labels=["q1", "q2", "q3", "q4"],
+                        counterclock=False,
+                        startangle=90,
                         autopct="%1.1f%%")
                 plt.axis('equal')
                 plt.title("Frequency ratio")
@@ -104,3 +104,7 @@ for i in range(1, len(sys.argv)):
 
         if data_list[-1] != 0 or data_list[-2] != 0:
             activeDays[int(date[-2:])] = 1
+
+    #outfile = open(sys.argv[i] + "-result.txt", 'w')
+    #for j in range(len(result)):
+    #    outfile.write(result[j][0] + ',' + result[j][1] + ',' + result[j][2] + ',' + result[j][3])
